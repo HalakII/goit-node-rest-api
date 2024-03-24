defb83cb5890db80ea75788a643adcfdf7826d6b
Iryna <1017imiia@gmail.com>
2024-03-24 13:09:39 +0200

    avatar update

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -7,7 +7,7 @@ const destination = path.resolve("temp");
 const storage = multer.diskStorage({
   destination,
   filename: (req, file, callback) => {
-    const uniquePrefix = `${Date.now()}- ${Math.round(Math.random() * 1e9)}`;
+    const uniquePrefix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
     const filename = `${uniquePrefix}_${file.originalname}`;
     callback(null, filename);
   },
